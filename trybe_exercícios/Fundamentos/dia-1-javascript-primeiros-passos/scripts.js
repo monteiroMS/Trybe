2280805563a2403922b7993564dffb39f8ccc349
Matheus Monteiro <matheuschran@hotmail.com>
2022-02-02 19:14:03 -0300

    parte 4

--- trybe_exercícios/Fundamentos/dia-1-javascript-primeiros-passos/scripts.js
+++ trybe_exercícios/Fundamentos/dia-1-javascript-primeiros-passos/scripts.js
@@ -25,14 +25,24 @@
 //    console.log(b);
 // }
 
-const a = 1002;
-const b = 895;
-const c = 890;
+// const a = 1002;
+// const b = 895;
+// const c = 890;
 
-if (a > b && a > c) {
-   console.log(a);
-} else if (b > a && b > c) {
-   console.log(b);
+// if (a > b && a > c) {
+//   console.log(a);
+// } else if (b > a && b > c) {
+//   console.log(b);
+// } else {
+//    console.log(c);
+// }
+
+const number = 0;
+
+if (number > 0) {
+    console.log("Positive");
+} else if (number < 0) {
+    console.log("Negative");
 } else {
-    console.log(c);
+    console.log("Zero");
 }
