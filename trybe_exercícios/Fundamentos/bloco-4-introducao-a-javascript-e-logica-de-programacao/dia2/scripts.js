534b261bc7001acce651616e625f42193b51fe13
Matheus Monteiro <matheuschran@hotmail.com>
2022-02-05 18:25:17 -0300

    exercicio 6

--- trybe_exercícios/Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia2/scripts.js
+++ trybe_exercícios/Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia2/scripts.js
@@ -45,12 +45,28 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 // exercicio 5 - realizado com ajuda do site https://pt.stackoverflow.com/questions/35202/como-saber-o-maior-valor-de-um-array
 
-let maior = 0;
+// let maior = 0;
+
+// for(index = 0; index < numbers.length; index += 1) {
+//     if (numbers[index] > maior) {
+//         maior = numbers[index];
+//     }
+// }
+
+// console.log(maior);
+
+// exercicio 6
+
+let impar = 0;
 
 for(index = 0; index < numbers.length; index += 1) {
-    if (numbers[index] > maior) {
-        maior = numbers[index];
+    if (numbers[index] % 2 !== 0) {
+        impar += + 1;
     }
 }
 
-console.log(maior);
+console.log(impar);
+
+if (impar === 0) {
+    console.log("Nenhum valor ímpar encontrado!");
+}
