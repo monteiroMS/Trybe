94001f0f089c249cadada29caaeeba0ae58791c0
Matheus Monteiro <matheuschran@hotmail.com>
2022-02-05 17:54:17 -0300

    exercicio 2

--- trybe_exercícios/Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia2/scripts.js
+++ trybe_exercícios/Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia2/scripts.js
@@ -3,3 +3,13 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 // exercicio 1
 
 console.log(numbers);
+
+// exercicio 2
+let soma = 0;
+
+for(index = 0; index < numbers.length; index += 1) {
+    soma += numbers[index];
+    console.log(numbers[index]);
+}
+
+console.log(soma);
