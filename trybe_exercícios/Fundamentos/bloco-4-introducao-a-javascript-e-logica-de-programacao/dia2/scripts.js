7f98763dcb89dc70eaa18e96680f8b2903577807
Matheus Monteiro <matheuschran@hotmail.com>
2022-02-05 18:16:15 -0300

    exercicios 5

--- trybe_exercícios/Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia2/scripts.js
+++ trybe_exercícios/Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia2/scripts.js
@@ -27,18 +27,30 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 // exercicio 4
 
-let soma = 0;
+// let soma = 0;
+
+// for(index = 0; index < numbers.length; index += 1) {
+//     soma += numbers[index];
+// }
+
+// let media = soma / numbers.length;
+
+// console.log(media);
+
+// if (media > 20) {
+//     console.log("Valor maior que 20");
+// } else {
+//     console.log("Valor menor ou igual a 20");
+// }
+
+// exercicio 5 - realizado com ajuda do site https://pt.stackoverflow.com/questions/35202/como-saber-o-maior-valor-de-um-array
+
+let maior = 0;
 
 for(index = 0; index < numbers.length; index += 1) {
-    soma += numbers[index];
+    if (numbers[index] > maior) {
+        maior = numbers[index];
+    }
 }
 
-let media = soma / numbers.length;
-
-console.log(media);
-
-if (media > 20) {
-    console.log("Valor maior que 20");
-} else {
-    console.log("Valor menor ou igual a 20");
-}
+console.log(maior);
