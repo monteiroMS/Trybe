6178aab9c3b69836bb99254805af3521265dc6d3
Matheus Monteiro <matheuschran@hotmail.com>
2022-02-08 10:29:05 -0300

    exercicio 6

--- trybe_exercícios/Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia4/scripts.js
+++ trybe_exercícios/Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia4/scripts.js
@@ -1,16 +1,16 @@
 // EXERCÍCIO 1
 
-let info = {
-    personagem: 'Margarida',
-    origem: 'Pato Donald',
-    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-  };
+// let info = {
+//     personagem: 'Margarida',
+//     origem: 'Pato Donald',
+//     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+//   };
 
 // console.log("Bem vinda, " + info.personagem);
 
 // EXERCÍCIO 2
 
-info.recorrente = 'Sim'
+// info.recorrente = 'Sim'
 
 // console.log(info);
 
@@ -28,20 +28,36 @@ info.recorrente = 'Sim'
 
 // EXERCÍCIO 5
 
-let info2 = {
-    personagem: 'Tio Patinhas',
-    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
-    nota: 'O último MacPatinhas',
-    recorrente: 'Sim'
+// let info2 = {
+//     personagem: 'Tio Patinhas',
+//     origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+//     nota: 'O último MacPatinhas',
+//     recorrente: 'Sim'
+//   };
+
+// for(let key in info) {
+//     for(let key2 in info2) {
+//         if (info[key] === info2[key2]) {
+//             console.log("Ambos recorrentes");
+//         } else if (key === key2) {
+//             console.log(info[key] + " e " + info2[key2]);
+//         }
+//     }
+// }
+
+// EXERCÍCIO 6 - feito com ajuda do gabarito do course
+
+let leitor = {
+    nome: 'Julia',
+    sobrenome: 'Pessoa',
+    idade: 21,
+    livrosFavoritos: [
+      {
+        titulo: 'O Pior Dia de Todos',
+        autor: 'Daniela Kopsch',
+        editora: 'Tordesilhas',
+      },
+    ],
   };
 
-for(let key in info) {
-    for(let key2 in info2) {
-        if (info[key] === info2[key2]) {
-            console.log("Ambos recorrentes");
-        } else if (key === key2) {
-            console.log(info[key] + " e " + info2[key2]);
-        }
-    }
-}
-
+  console.log("O livro favorito de " + leitor.nome + " " + leitor.sobrenome + " se chama " + "'" + leitor['livrosFavoritos'][0]['titulo'] + "'");
