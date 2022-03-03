973e00619d8ab8aa2d7c46cfa39c2284a440bb3e
Matheus Monteiro <matheuschran@hotmail.com>
2022-03-03 18:39:16 -0300

    exercicio 2

--- trybe_exercícios/Fundamentos/bloco7/dia1/script.js
+++ trybe_exercícios/Fundamentos/bloco7/dia1/script.js
@@ -1,12 +1,23 @@
-const testingScope = escopo => {
-    if (escopo === true) {
-      let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
-      ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
-      console.log(ifScope);
-    } else {
-      let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
-      console.log(elseScope);
-    }
-  }
+// EXERCÍCIO 1
 
-  testingScope(false);
+// const testingScope = escopo => {
+//     if (escopo === true) {
+//       let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+//       ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
+//       console.log(ifScope);
+//     } else {
+//       let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+//       console.log(elseScope);
+//     }
+//   }
+
+//   testingScope(false);
+
+// EXERCÍCIO 2
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+const compare = (a, b) => a - b;
+oddsAndEvens.sort(compare);
+
+console.log(`Os números ${oddsAndEvens} se encontram ordenados de forma crescente!`);
