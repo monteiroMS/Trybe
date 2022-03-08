9fb7e5be2ac5e87055a880796e013eb78ad39edd
Matheus Monteiro <matheuschran@hotmail.com>
2022-03-08 16:00:05 -0300

    exercicio 1

--- trybe_exercícios/Fundamentos/bloco7/dia3/sum.js
+++ trybe_exercícios/Fundamentos/bloco7/dia3/sum.js
@@ -1,4 +1,8 @@
-// sum.js
-const sum = (a, b) => a + b;
-
+const sum = (a, b) => {
+  if(typeof a !== 'number' || typeof b !== 'number') {
+    throw new Error('parameters must be numbers');
+  } else {
+    return a + b;
+  }
+}
 module.exports = sum;
