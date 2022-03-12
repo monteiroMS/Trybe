599433c32f09824b13087df78aecbaca1aa57e98
Matheus Monteiro <matheuschran@hotmail.com>
2022-03-12 17:21:53 -0300

    exercicio 1

---
+++ trybe_exercícios/Fundamentos/bloco8/dia1/script.js
@@ -0,0 +1,15 @@
+const createEmail = (name) => {
+  const emailName = name.toLowerCase().split(' ').join('_');
+  return `${emailName}@trybe.com`;
+}
+
+const newEmployees = () => {
+  const employees = {
+    id1: createEmail('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+    id2: createEmail('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+    id3: createEmail('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+  }
+  return employees;
+};
+
+console.log(newEmployees());
