335b47d20ebc26302bc61b7291c018625bdf6b0c
Matheus Monteiro <matheuschran@hotmail.com>
2022-03-14 13:44:47 -0300

    exercicio bonus parte II

--- trybe_exercícios/Fundamentos/bloco8/dia1/bonus.js
+++ trybe_exercícios/Fundamentos/bloco8/dia1/bonus.js
@@ -2,38 +2,77 @@ const mage = {
   healthPoints: 130,
   intelligence: 45,
   mana: 125,
-  damage: () => (
-    {
-      dano: () => (
-        Math.floor(Math.random() * ((mage.intelligence * 2) - mage.intelligence) + mage.intelligence)
-      ), 
-      manaSpent: () => {
-        if (mage.mana < 15) {
-          return 'Não possui mana suficiente';
-        } else {
-          mage.mana -= 15;
-        }
-      },
-    }
-  ),
+  damage: undefined,
 };
 
 const warrior = {
   healthPoints: 200,
   strength: 30,
   weaponDmg: 2,
-  damage: () => (
-    Math.floor(Math.random() * ((warrior.weaponDmg * warrior.strength) - warrior.strength)) + warrior.strength
-  ),
+  damage: undefined,
 };
 
 const dragon = {
   healthPoints: 350,
   strength: 50,
-  damage: () => (
-    Math.floor(Math.random() * (dragon.strength - 15)) + 15
-  ),
+  damage: undefined,
 };
+
+const damageMago = () => (
+  Math.floor(Math.random() * ((mage.intelligence * 2) - mage.intelligence) + mage.intelligence)
+)
+
+const manaSpentMago = () => {
+  if (mage.mana < 15) {
+    console.log('Mago não possui mana suficiente');
+    return 0;
+  } else {;
+    return 15;
+  }
+}
+
+const danoMago = () => (
+  { dano: damageMago(), manaSpent: manaSpentMago() }
+);
+
+const danoGuerreiro = () => (
+  Math.floor(Math.random() * ((warrior.weaponDmg * warrior.strength) - warrior.strength)) + warrior.strength
+);
+
+const danoDragao = () => (
+  Math.floor(Math.random() * (dragon.strength - 15)) + 15
+)
 
 const battleMembers = { mage, warrior, dragon };
 
+const gameActions = {
+  warriorTurn: (callback) => {
+    const danoWarrior = callback();
+    dragon.healthPoints -= danoWarrior;
+    warrior.damage = danoWarrior;
+  },
+  mageTurn: (callback) => {
+    const danoMage = callback();
+    if (danoMage.manaSpent === 0) {
+      dragon.healthPoints -= 0;
+      mage.damage = 0;
+      mage.mana -= 0;
+    } else {
+      dragon.healthPoints -= danoMage.dano;
+      mage.damage = danoMage.dano;
+      mage.mana -= danoMage.manaSpent;
+    }
+  },
+  dragonTurn: (callback) => {
+    const danoDragon = callback();
+    mage.healthPoints -= danoDragon;
+    warrior.healthPoints -= danoDragon;
+    dragon.damage = danoDragon;
+  },
+  result: () => console.log(battleMembers),
+};
+
+gameActions.warriorTurn(danoGuerreiro);
+gameActions.mageTurn(danoMago);
+gameActions.dragonTurn(danoDragao);
+gameActions.result();
